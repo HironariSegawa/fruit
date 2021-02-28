6cc2bf91a77249754c64a9ecafa4a0911b2b5f66
rinarohi <78575465+HironariSegawa@users.noreply.github.com>
2021-02-28 12:02:09 +0900

    Create fruit.js

---
+++ fruit.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+    speed: 2000,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+})
